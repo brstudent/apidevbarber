cd988811ec969da158c77d0b3634db5e75595f92
Veronica de Lima <lindapaineira@gmail.com>
2021-01-19 17:37:38 -0300

    Creating Controllers and Models

---
+++ app/Models/BaberTestimonial.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BaberTestimonial extends Model
+{
+    use HasFactory;
+
+    protected $table = 'babertestimonials';
+    public $timestamps = false;
+}
